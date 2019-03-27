f9b759357c8fa534bd498668b71f0cf361003abe
Tomas Vink <tv@eenwereldmetlef.be>
2019-03-27 15:14:18 +0100

    dependancy update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Display and sort data in admin list view',
-  version: '3.0.0',
+  version: '3.0.1',
   name: 'lef:adminlist',
   git: 'https://github.com/LEFapps/lef-adminlist'
 })
@@ -11,5 +11,6 @@ Package.onUse(api => {
 })
 
 Npm.depends({
+  '@fortawesome/react-fontawesome': '0.1.0',
   'prop-types': '15.6.2'
 })
