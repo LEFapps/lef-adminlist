01abb99e8148572231d08773f4d296a92acaa569
johanlef <jb@eenwereldmetlef.be>
2019-08-02 16:33:01 +0200

    Version bump
    3.0.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Display and sort data in admin list view',
-  version: '3.0.6',
+  version: '3.0.7',
   name: 'lef:adminlist',
   git: 'https://github.com/LEFapps/lef-adminlist'
 })
